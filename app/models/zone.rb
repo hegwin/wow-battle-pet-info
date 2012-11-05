4dfea1cb49c5691334fd6b73a29e0e8f2a49ae0e
hegwin <zwt315@163.com>
2012-11-05 18:11:07 +0800

    add basic validation for zones

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,3 +1,6 @@
 class Zone < ActiveRecord::Base
   attr_accessible :blz_id, :description, :parent_id, :title_cn, :title_en
+
+  validates :blz_id, :title_cn, presence: true
+  validates :blz_id, :title_cn, uniqueness: true
 end
