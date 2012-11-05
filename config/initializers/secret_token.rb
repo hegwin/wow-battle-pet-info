74302df5f4be07dfe7a00587d97583319f0c283a
hegwin <zwt315@163.com>
2012-11-05 16:16:52 +0800

    initialize the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WowPetBattleInfo::Application.config.secret_token = 'ec9d106dc7bb590ad496a5036467bc3e0f60dbde6717d585b552620bc08b4a8e8e3e9ad50667a13049ab366675918bb237206952a829364a7a5b345603f47723'
