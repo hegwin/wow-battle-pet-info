538b928bd6b4b58d06b9330a18a1a02ef1d658c0
hegwin <zwt315@163.com>
2012-11-06 13:25:16 +0800

    add simple unit test for zone model

--- test/unit/zone_test.rb
+++ test/unit/zone_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ZoneTest < ActiveSupport::TestCase
+  fixtures :zones
   # test "the truth" do
   #   assert true
   # end
+
+  test "empty zone should be invalid" do
+    zone = Zone.new
+    assert zone.invalid?
+    assert zone.errors[:blz_id].any?
+    assert zone.errors[:title_cn].any?
+  end
+  
+  test "chinese title and blzzard id should be unique" do
+    zone = Zone.new(blz_id:      zones(:grey_valley).blz_id,
+                    title_cn:    zones(:the_barren).title_cn,
+                    title_en:    "Mysteria",
+                    description: "Place of Mists",
+                    parent_id:   1)
+    assert !zone.save
+    assert zone.errors[:blz_id].any?
+    assert zone.errors[:title_cn].any?
+  end
+
 end
