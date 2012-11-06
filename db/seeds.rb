03f5bdcb0ea990ca6f1939bca0d69de802520e72
hegwin <zwt315@163.com>
2012-11-06 12:51:09 +0800

    add the zone source data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Zone.transaction do
+  Zone.delete_all
+    
+  CSV.foreach("#{Rails.root}/db/data_src/zones.csv") do |blz_id, title_cn, parent_id|
+    zone = Zone.new(blz_id: blz_id, title_cn: title_cn)
+    if parent_id =~ /\d/
+      parent_zone = Zone.find_by_blz_id(parent_id)
+      zone.parent_id = parent_zone.id
+    end
+    zone.save
+  end
+end
