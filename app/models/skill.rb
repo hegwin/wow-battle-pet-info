86eecc8910085a79041b6caf5f35017025253855
hegwin <zwt315@163.com>
2012-11-14 18:50:11 +0800

    update the association in skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
   attr_accessible :blz_id, :category, :cd, :comments, :description, :hit_rate, :status, :title_cn, :title_en
 
-  has_and_belongs_to_many :pets
+  has_many :acquirings
   belongs_to :category
 end
