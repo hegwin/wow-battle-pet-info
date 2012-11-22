70e0b51cd6f23dfa25c61f2b1819134633841c10
hegwin <zwt315@163.com>
2012-11-22 19:43:17 +0800

    add a unit test to pets, correct spelling mistakes in pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,8 @@
 class Pet < ActiveRecord::Base
   attr_accessible :blz_id, :category, :description, :event, :icon_url, :nga_url, :season, :source, :status, :title_cn, :title_en, :url_param, :weather, :reviewed
 
-  validate :blz_id, :title_cn, :source, presence: true
-  validate :blz_id, uniqueness: true
+  validates :blz_id, :title_cn, :source, presence: true
+  validates :blz_id, uniqueness: true
 
   has_and_belongs_to_many :zones
   has_many :acquirings
--- test/unit/pet_test.rb
+++ test/unit/pet_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
+  fixtures :pets
   # test "the truth" do
   #   assert true
   # end
 
+  test "pet attributes must not be empty" do
+    pet = Pet.new
+    assert pet.invalid?
+    assert pet.errors[:blz_id].any?
+    assert pet.errors[:title_cn].any?
+    assert pet.errors[:source].any?
+  end
 
   test "blizzard id must be unique" do
-
+    flunk
   end
 end
