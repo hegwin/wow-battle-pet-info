255672a479eaea6e1007e339f287592318ae866a
hegwin <zwt315@163.com>
2012-11-30 18:31:21 +0800

    hide the edit/destroy for pets and skills without admin, add an integration test

--- test/integration/user_and_login_test.rb
+++ test/integration/user_and_login_test.rb
@@ -1,11 +1,46 @@
 require 'test_helper'
 
 class UserAndLoginTest < ActionDispatch::IntegrationTest
-  test "create the first user" do
-    flunk
-    User.delete_all
-    assert_equal 0, User.count
-    get '/users/new'
+  test "a normal guest come" do
+    pet = pets(:hatchling)
+    get '/pets'
     assert_response :success
+    assert_select 'div.admin-btn-panel', 0
+    assert_select 'ul.nav li', 8
+    get "pets/#{pet.id}"
+    assert_response :success
+  end
+  
+  test "an administrator's work flow" do
+    admin = users(:ashuram)
+    pet = pets(:hatchling)
+    get "pets/#{pet.id}"
+    assert_response :success
+    assert_select 'div.page-header h1', /#{pet.title_cn}/
+    assert_select 'div.admin-btn-panel', 0
+    
+    get "pets/#{pet.id}/edit"
+    assert_redirected_to login_url
+    
+    post_via_redirect login_url, {name: "ashuram", password: "pswd"}
+    assert_response :success
+    assert_equal admin.id, session[:user_id]
+    assert_select 'ul.nav li', 10
+    assert_select 'div.admin-btn-panel'
+    get "pets/#{pet.id}"
+    assert_select 'div.admin-btn-panel'
+    
+    get "pets/#{pet.id}/edit"
+    assert_response :success
+
+    put_via_redirect pet_path(pet), id: pet, pet: {title_cn: pet.title_cn, reviewed: "0" }
+    assert_response :success
+    assert_select "div.page-header h1 label", "Unreviewed"
+
+    delete_via_redirect "/logout"
+    assert_response :success
+    assert_equal nil, session[:user_id]
+    get "/pets"
+    assert_select "ul.nav li", 8
   end
 end
