5ae685fa6feeb784c01ab47c658bcc14e29fb60a
hegwin <zwt315@163.com>
2013-03-06 13:13:55 +0800

    [#45459655] Add a function to search pet by zones

--- lib/pet_search.rb
+++ lib/pet_search.rb
@@ -1,5 +1,5 @@
 class PetSearch < SearchModel
-  attr_reader :title, :category, :source
+  attr_reader :title, :category, :source, :zone
 
   def execute
     to_find_options(Pet).order("pets.title_cn")
@@ -9,5 +9,6 @@ class PetSearch < SearchModel
     add_search_condition("pets.title_cn", "any", title) if title.present?
     add_search_condition("pets.source", "equal", source) if source.present?
     add_include_condition(:category, "categories.id = ?", category) if category.present?
+    add_include_condition(:zones, "zones.title_cn ilike ?", zone) if zone.present?
   end
 end
